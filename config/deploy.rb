e16861439ef47d5511fb5939a4a19c4d74bd723f
Danil Pismenny <danil@orionet.ru>
2012-07-23 15:26:13 +0400

    Update example deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,21 +16,23 @@ current_branch = `git branch 2>/dev/null | sed -e "/^\s/d" -e "s/^\*\s//"`.chomp
 namespace :vlad do
   def skip_scm; false; end
 
-  set :application, "blogs.investcafe.ru"
+  set :application, "brandymint.ru"
 
   set :rails_env, ENV['DEPLOY_TO'] || "icf"
 
   if ENV['DEPLOY_TO']=='production'
     set :revision, 'origin/HEAD/production'
-    set :domain, "wwwdata@investcafe.ru"
-  else
+    set :domain, "wwwdata@#{application}"
+  elsif ENV['DEPLOY_TO']=='stage'
     set :revision, "origin/HEAD/#{current_branch}"
-    set :domain, "wwwdata@icfdev.ru"
+    set :domain, "wwwdata@stage.#{application}"
   end
 
-  set :deploy_to, "/home/wwwdata/blogs.investcafe.ru"
+  set :deploy_to, "/home/wwwdata/#{application}"
   set :keep_releases,	3
-  set :repository, 'git@github.com:investcafe/blogs.investcafe.ru.git'
+  set :repository, "git@github.com:BrandyMint/#{application}.git"
+
+  set :rake_cmd, 'bundle exec rake'
 
   set :copy_files, [ 'config/database.yml' ]
   set :symlinks, copy_files
@@ -46,12 +48,10 @@ namespace :vlad do
 
   desc "Put revision into public/revision"
   remote_task :put_revision do
-    # #{source.revision(revision)}
     airbrake =  "cd #{current_release}; nohup bundle exec rake airbrake:deploy RAILS_ENV=#{rails_env} TO=#{rails_env} REVISION=#{revision} USER=`whoami` REPO=#{repository} >> ./tmp/airbrake_notify.log &"
     run "cd #{scm_path}/repo; git rev-parse HEAD > #{release_path}/public/revision; #{airbrake}"
   end
 
-  # before :"deploy:symlink", :"deploy:assets";
   desc 'Precompile assets'
   remote_task :precompile do
     puts "Precompile.."
@@ -63,14 +63,6 @@ namespace :vlad do
     puts "Foreverb.."
     run "cd #{current_path}; RAILS_ENV=#{rails_env} nohup bundle exec ./script/foreverb-cron >> ./tmp/forever-restart.log"
   end
-
-  desc 'Load new db/templates and pages'
-  remote_task :load_templates_and_pages do
-    puts 'Load templates'
-    run "cd #{current_path}; RAILS_ENV=#{rails_env} nohup bundle exec rake icf:templates:load icf:pages:load >> ./tmp/load_templates_and_pages.log &"
-  end
-
-  set :rake_cmd, 'bundle exec rake'
 
   namespace :unicorn do
 
@@ -88,30 +80,32 @@ namespace :vlad do
 
   end
 
-  namespace :mybundle do
-    set :bundle_without, "development test"
-
-    desc "Execute bundle --deployment" #
-    remote_task :install=>['vlad:rvm:trust:release', 'vlad:rvm:trust:current'] do
-      # run bundle install with explicit path and without test dependencies
-      # С release_path не работает текущая git версия
-      run "cd #{current_path}; bundle install --deployment --without #{bundle_without}"
-    end
-  end
-
    set :deploy_tasks, %w[
            vlad:update
            vlad:symlink
            vlad:bundle:install
            vlad:migrate
            vlad:put_revision
-           vlad:load_templates_and_pages
-           vlad:foreverb
            vlad:precompile
            vlad:unicorn:upgrade
-           vlad:delayed_job:restart
            vlad:cleanup
    ]
+
+
+   # Long story
+   #
+   #set :deploy_tasks, %w[
+           #vlad:update
+           #vlad:symlink
+           #vlad:bundle:install
+           #vlad:migrate
+           #vlad:put_revision
+           #vlad:foreverb
+           #vlad:precompile
+           #vlad:unicorn:upgrade
+           #vlad:delayed_job:restart
+           #vlad:cleanup
+   #]
 end
 
 
